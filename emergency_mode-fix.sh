02eafd2246597e3554599e120a8dbe3f60993f3c
christanplaza <christan.shane.plaza@sun-asterisk.com>
2020-09-26 14:38:27 +0800

    Fix V1

---
+++ emergency_mode-fix.sh
@@ -0,0 +1,17 @@
+sudo apt update
+
+sudo apt install build-essential
+
+wget http://downloads.sourceforge.net/project/e2fsprogs/e2fsprogs/v1.43.1/e2fsprogs-1.43.1.tar.gz
+
+tar xzf e2fsprogs-1.43.1.tar.gz
+
+cd e2fsprogs-1.43.1
+
+./configure
+
+make
+
+cd e2fsck/
+
+sudo ./e2fsck -y /dev/sda7/
